aa96498ca1aeec24b57616fd64f722a797cb2d3c
Bycarkos <cbonedriera@gmail.com>
2025-04-30 11:59:35 +0200

    Starting Point Coloring

---
+++ Coloring/dataloaders/dataset.py
@@ -0,0 +1,16 @@
+import torch
+from torch.utils.data import Dataset
+from PIL import Image
+import os
+from torchvision import transforms
+
+
+class ColorizationDataset(Dataset):
+    def __init__(self, image_dir, transform=None):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __getitem__(self, idx):
+        pass
